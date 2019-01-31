1ee34038c7ea17aa1759bbdf05f59d4b8df8a504
ViktoriaMart <47144982+ViktoriaMart@users.noreply.github.com>
2019-01-31 20:49:47 +0200

    Added DAO and BUS

---
+++ BUS/OrderBUS.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using DAO;
+using Models;
+
+namespace BUS
+{
+    public class OrderBUS
+    {
+        OrderDAO orderDAO;
+
+        public OrderBUS()
+        {
+            orderDAO = new OrderDAO();
+        }
+
+        public ObservableCollection<OrderProduct> GetOrders()
+        {
+            var ordersCollection = orderDAO.ReadOrders();
+            ObservableCollection<OrderProduct> collection = new ObservableCollection<OrderProduct>();
+
+            foreach (var item in ordersCollection)
+            {
+                collection.Add(item);
+            }
+
+            if (collection.Count == 0)
+            {
+                throw new Exception("Немає активних замовлень");
+            }
+            return collection;
+        }
+
+        public double GetSum(ObservableCollection<OrderProduct> collection)
+        {
+            double sum = 0;
+            
+            foreach (var item in collection)
+            {
+                sum += item.ProductPrice;
+            }
+
+            if (sum <= 0)
+            {
+                throw new Exception("Неправильно введені дані");
+            }
+
+            return sum;
+        }
+
+        public void RemoveAll(IList list)
+        {
+            while (list.Count > 0)
+            {
+                list.RemoveAt(list.Count - 1);
+            }
+        }
+    }
+}
---
+++ DAO/OrderDao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using System.IO;
+
+namespace DAO
+{
+    public class OrderDAO
+    {
+        public List<OrderProduct> ReadOrders()
+        {
+            string line;
+            List<OrderProduct> collection = new List<OrderProduct>();
+
+            using (StreamReader sr = new StreamReader("orders.txt", System.Text.Encoding.Default))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    collection.Add((new OrderProduct().ReadOrder(line)) as OrderProduct);
+                }
+            }
+
+            return collection;
+        }
+    }
+}
